ea98ca821c62ed2fcf898b0b8d645a5a441bc8af
Anu-osi <anudeepgoude@gmail.com>
2025-06-06 19:01:24 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const typed = new Typed('#typed-text', {
-  strings: [
-    "Cybersecurity Professional",
-    "IT Support Expert",
-    "IoT Intern - Virus Mitigation",
-    "TryHackMe Top 1.5%",
-    "Future AI-infused Security Founder"
-  ],
-  typeSpeed: 50,
-  backSpeed: 30,
-  loop: true
-});
+
